63b3a4ed815ac8439795f4d99d15d457f89fabc5
seanjasm <seanjasm@gmail.com>
2019-04-25 20:41:35 -0400

    First commit

---
+++ PowersOfNumber/PowersOfNumber/MethodLibrary.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowersOfNumber
+{
+    class MethodLibrary
+    {
+        public static string GetUserInput(string message)
+        {
+            Console.Write($"\n{message} ");
+            return Console.ReadLine();
+        }
+
+        public static int ParseUserInputAsInteger(string input, string message = "")
+        {
+            if (int.TryParse(input, out int result))
+            {
+                return result;
+            }
+            else
+            {
+                return ParseUserInputAsInteger(GetUserInput($"There was an error! Try again. {message}"));
+            }
+        }
+
+        public static void Display(string message)
+        {
+            Console.Write($"{message, -15}");
+        }
+
+        public static int SquaresOfNumber(int number)
+        {
+             return number * number;
+        }
+
+        public static int CubeOfNumber(int number)
+        {
+            return number * number * number;
+        }
+
+        public static bool Confirmation(string message, string trueOption, string falseOption = "")
+        {
+            if (trueOption.ToLower().StartsWith(GetUserInput(message), StringComparison.InvariantCultureIgnoreCase))
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
---
+++ PowersOfNumber/PowersOfNumber/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowersOfNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool _continue = true;
+
+            while (_continue)
+            {
+                int input = MethodLibrary.ParseUserInputAsInteger(MethodLibrary.GetUserInput("Give me a number: "),
+                    "I need a number: ");
+
+                Console.WriteLine("\n\n");
+                MethodLibrary.Display("Number");
+                MethodLibrary.Display("Square");
+                MethodLibrary.Display("Cube");
+                Console.WriteLine();
+                MethodLibrary.Display("==========");
+                MethodLibrary.Display("==========");
+                MethodLibrary.Display("==========");
+
+                Console.WriteLine("\n");
+
+                for (int i = 1; i <= input; i++)
+                {
+                    MethodLibrary.Display(i.ToString());
+                    MethodLibrary.Display(MethodLibrary.SquaresOfNumber(i).ToString());
+                    MethodLibrary.Display(MethodLibrary.CubeOfNumber(i).ToString());
+                    Console.WriteLine("\n");
+                }
+
+                _continue = MethodLibrary.Confirmation("<Y> to exit: ", "y");
+            }
+        }
+    }
+}
